c64b1c548517ef3cedcffeb83106359a0f040e5e
赵仁建 <zhaorenjian@bytedance.com>
2021-10-20 16:10:52 +0800

    docs: 增加静态站点部署

--- .umirc.ts
+++ .umirc.ts
@@ -4,8 +4,10 @@ export default defineConfig({
   title: 'awesome-react-mobile',
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
-  // more config: https://d.umijs.org/config
+  base: process.env.NODE_ENV === 'production' ? '/awesome-react-mobile/' : '/',
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/awesome-react-mobile/' : '/',
+  exportStatic: {},
 });
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # awesome-react-mobile
 
-在做 react h5 组件库时做的比较有意思的俩个组件,一个纯原创（[Cascade](https://nsfi.github.io/fishd-mobile-site/index.html#/zh-CN/components/cascade)），一个半借鉴（[Dropdown](https://nsfi.github.io/fishd-mobile-site/index.html#/zh-CN/components/dropdown) [Vant Vue](https://youzan.github.io/vant/#/zh-CN/dropdown-menu)）
+> 记录一些常用 h5 组件
 
-这俩个组件都比较独立，当时做的时候市面上的 react mobile 组件库都没有相应的效果，因此特别抽出来
+https://zrj1031.github.io/awesome-react-mobile/
